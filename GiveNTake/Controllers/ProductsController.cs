5c7d7bb5091fc9643beaec561468d8b06277e078
CJRivera <carisua@verizon.net>
2019-10-26 15:34:41 -0400

    Basic MVC with API

---
+++ GiveNTake/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiveNTake.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("search/{category}/{subcategory=all}")]
+        public string[] GetProducts(string category, string subcategory) {
+            return new [] {
+                   $"Category: {category} SubCategory: {subcategory}"
+                };
+        }
+    }
+}
--- GiveNTake/Startup.cs
+++ GiveNTake/Startup.cs
@@ -16,6 +16,8 @@ namespace GiveNTake
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +30,18 @@ namespace GiveNTake
 
             app.UseRouting();
 
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
